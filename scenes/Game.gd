4c86d9ca308629b83f83aaa53e861ecc327fdf69
Lea Taylor <80832630+Lea-poh@users.noreply.github.com>
2025-12-07 21:58:11 +0100

    Emoji change

--- scenes/Game.gd
+++ scenes/Game.gd
@@ -26,8 +26,8 @@ func start_game():
 	timer.start()
 
 func _process(delta):
-	timer_label.text = "⏱️: " + str(int(timer.time_left))
-	score_label.text = "🌼: " + str(flowers_collected)
+	timer_label.text = "Timer: " + str(int(timer.time_left))
+	score_label.text = "Flowers: : " + str(flowers_collected)
 
 func add_flower():
 	flowers_collected += 1
